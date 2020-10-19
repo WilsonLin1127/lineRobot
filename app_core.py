4c7355e11bf3f89937aa6c32b6bef8425c49d464
WilsonLin1127 <47200432+WilsonLin1127@users.noreply.github.com>
2020-10-19 16:27:22 +0800

    Update app_core.py

--- app_core.py
+++ app_core.py
@@ -10,8 +10,8 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSend
 app = Flask(__name__)
 
 # LINE 聊天機器人的基本資料
-line_bot_api = LineBotApi('')
-handler = WebhookHandler('')
+line_bot_api = LineBotApi('YOUR_LineBot_Channel access token')
+handler = WebhookHandler('YOUR_LineBot_Channel secret')
 
 
 # 接收 LINE 的資訊
