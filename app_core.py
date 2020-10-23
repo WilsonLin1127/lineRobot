431045f279e9707634d984e6bbafe93e0bc318c2
WilsonLin1127 <47200432+WilsonLin1127@users.noreply.github.com>
2020-10-23 09:14:38 +0800

    Update app_core.py

--- app_core.py
+++ app_core.py
@@ -4,14 +4,15 @@ import re
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-
+from imgurpython import ImgurClient
+import random
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
 
 app = Flask(__name__)
 
 # LINE 聊天機器人的基本資料
-line_bot_api = LineBotApi('YOUR_LineBot_Channel access token')
-handler = WebhookHandler('YOUR_LineBot_Channel secret')
+line_bot_api = LineBotApi('Line的Cahannel access token')
+handler = WebhookHandler('Line的Channel Secret')
 
 
 # 接收 LINE 的資訊
@@ -37,16 +38,25 @@ def echo(event):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=event.message.text)
-        )
+        )	
     '''
-    #給一張圖
-    if re.search(r"(牛|厲害|強|猛)", event.message.text, re.DOTALL):
+   
+    if re.search(r"(牛|奶)", event.message.text, re.DOTALL):
+        clientTemp= ImgurClient('Imgur的clientid', 'Imgur的clientSecreat')
+        images = clientTemp.get_album_images('Imgur Album Hash')
+        imageUrls = []
+	
+        for image in images:
+                imageUrls.append(image.link)
+        randomNum = random.randint(1, imageUrls.__len__())
+        imgUrl = imageUrls[randomNum - 1]
+        
         message = ImageSendMessage(
-            original_content_url='https://i.imgur.com/PPJOlv8.jpg',
-            preview_image_url='https://i.imgur.com/PPJOlv8.jpg'
+                original_content_url=imgUrl,
+                preview_image_url=imgUrl
         )
         line_bot_api.reply_message(event.reply_token, message)
         return 0
-    
+
 if __name__ == "__main__":
     app.run()
